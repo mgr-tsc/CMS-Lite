f94a3cdbf3b010bdad171f62b85f0b4ca30baac8
marcoguevararodriguez <marco2029guerod@outlook.com>
2025-09-18 20:12:00 -0400

    Work in progress

---
+++ CmsLiteWebAppClient/Components/NamespaceHelper.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace CmsLiteWebAppClient.Components
+{
+    // This helps establish the namespace for IntelliSense
+    public partial class App : ComponentBase
+    {
+        // This is a partial class that will be merged with the generated App component from App.razor
+    }
+}
